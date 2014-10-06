3c7b9bfb5418bcc4e7f276eaf97d0954c7a20c12
Jeff Paine <JeffPaine@users.noreply.github.com>
2014-10-06 19:37:35 -0400

    Update intro documentation / comments

--- csvsplit.go
+++ csvsplit.go
@@ -1,17 +1,21 @@
 /*
 csvsplit: Split a .csv file into multiple files with a given number of records per file.
 
-Usage:
-	csvsplit -r records -i input [-o path]
+Usage
+    # Basic usage
+    $ csvsplit -r <number of records> -i <input file> [-o path]
 
-Arguments:
-	-r: Number of records per file
-	-i: Filename of the input file to split
-	-o: filename / path of the file output (optional)
+    # Split file.csv into files with 300 records a piece
+    $ csvplit -r 300 -i file.csv
 
-Examples:
-	# Split file.csv into files with 300 records a piece
-	$ csvplit -r 300 -i file.csv
+    # Split file.csv into files with 37 records a piece into the subfolder 'stuff'
+    $ csvplit -r 37 -i file.csv -o stuff/
+
+Arguments
+    -r: Number of records per file
+    -i: Filename of the input file to split
+    -o: filename / path of the file output (optional)
+
 */
 package main
 
