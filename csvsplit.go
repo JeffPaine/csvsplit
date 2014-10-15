7e0da351d76580971fe84306fb8d39cbb0c5313a
Jeffrey Paine <jeffreyalanpaine@gmail.com>
2014-10-14 20:45:26 -0400

    Refactor main() Closes #4

--- csvsplit.go
+++ csvsplit.go
@@ -19,7 +19,7 @@ var (
 
 func main() {
 	flag.Parse()
-	if *flagInput == "" || *flagRecords < 1 || *flagHeaders < 0 {
+	if *flagInput == "" || *flagRecords < 1 || *flagHeaders < 0 || *flagHeaders >= *flagRecords {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -31,7 +31,6 @@ func main() {
 	defer csvFile.Close()
 
 	reader := csv.NewReader(csvFile)
-	headers := make([][]string, 0)
 	records := make([][]string, 0)
 	fileCount := 1
 	for {
@@ -42,24 +41,19 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if len(headers) < *flagHeaders {
-			headers = append(headers, record)
-			continue
-		}
-
 		records = append(records, record)
-		if len(records) == *flagRecords-*flagHeaders {
-			saveCSVFile(headers, records, fileCount)
-			records = make([][]string, 0)
+		if len(records) == *flagRecords {
+			saveCSVFile(records, fileCount)
+			records = records[:*flagHeaders]
 			fileCount += 1
 		}
 	}
 	if len(records) > 0 {
-		saveCSVFile(headers, records, fileCount)
+		saveCSVFile(records, fileCount)
 	}
 }
 
-func saveCSVFile(h [][]string, r [][]string, fileCount int) {
+func saveCSVFile(r [][]string, fileCount int) {
 	fileName := fmt.Sprintf("%v%03d%v", *flagOutput, fileCount, ".csv")
 	if _, err := os.Stat(fileName); err == nil {
 		log.Fatal("File exists: ", fileName)
@@ -70,8 +64,5 @@ func saveCSVFile(h [][]string, r [][]string, fileCount int) {
 	}
 	defer f.Close()
 	writer := csv.NewWriter(f)
-	if len(h) > 0 {
-		writer.WriteAll(h)
-	}
 	writer.WriteAll(r)
 }
